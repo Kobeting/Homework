ecf55df6d61744446bf6844df82c834c11c3c90e
Kobeting <at850528@gmail.com>
2019-01-16 16:52:23 +0800

    Lab4:parser complete

---
+++ NTCU_PDA/PDA_Lab4/main.cpp
@@ -0,0 +1,10 @@
+#include "parser.h"
+
+
+int main(int argc, char* argv[]){
+
+	Parser parser;
+	data_C data;
+	
+	parser.parser(argv[1],data);
+}
---
+++ NTCU_PDA/PDA_Lab4/parser.cpp
@@ -0,0 +1,53 @@
+#include "parser.h"
+
+using namespace std;
+
+//-------------------------------------------------------------------------------------------------------//
+void Parser::parser(const char* FileName, data_C& data){
+	ifstream input;
+    input.open(FileName, ios::in);
+
+	if (!input){
+        cout << "File " << FileName << " Can Not Be Opened! Exit" << endl;
+        return;
+    }
+    else{
+    	cout << "[Parser] - File " << FileName << " Open Success." << endl;
+        cout << "[Parser] - Parsing ... " << endl;
+    }
+
+    stringstream ss;
+    string line, word;
+    getline(input,line);
+
+    ss.str("");
+    ss.clear();
+    ss << line;
+    while(1){
+    	ss >> word;
+    	if(ss.fail()) break;
+    	column_S column;
+    	column.t = stoi(word);
+    	data.pins.push_back(column);
+    }
+
+    getline(input,line);
+
+    ss.str("");
+    ss.clear();
+    ss << line;
+    int ColumnIter = 0;
+    while(1){
+    	ss >> word;
+    	if(ss.fail()) break;
+    	data.pins[ColumnIter++].b = stoi(word);
+    }
+
+    // for(int i=0; i<data.pins.size(); i++)
+    // 	cout << data.pins[i].t << ";";
+    // cout << endl;
+    // for(int i=0; i<data.pins.size(); i++)
+    // 	cout << data.pins[i].b << ";";
+    cout << "[Parser] - Parsing Complete!" << endl;
+}
+//-------------------------------------------------------------------------------------------------------//
